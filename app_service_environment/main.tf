a58ddf408982f213bf7d58077e784ff70bfeb9c7
vongani <vonganifm@gmail.com>
2024-08-08 18:32:49 +0200

    more

---
+++ app_service_environment/main.tf
@@ -0,0 +1,7 @@
+
+resource "azurerm_app_service_environment_v3" "this" {
+  name                = var.app_service_environment_name
+  resource_group_name = var.app_service_environment_rg_name
+  subnet_id           = var.app_service_environment_subnet_id
+  zone_redundant = var.app_service_environment_zone_reduntant
+}
---
+++ app_service_environment/variables.tf
@@ -0,0 +1,16 @@
+variable "app_service_environment_name" {
+  type = string
+  description = ""
+}
+variable "app_service_environment_rg_name" {
+  type = string
+  description = ""
+}
+variable "app_service_environment_subnet_id" {
+  type = string
+  description = ""
+}
+variable "app_service_environment_zone_reduntant" {
+  type = string
+  description = ""
+}
